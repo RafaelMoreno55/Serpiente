981354b48842d20272e3f5da0d950a2cdca22263
rafael torres moreno <rafael201_33@hotmail.com>
2021-05-29 17:27:19 -0500

    Se realizó el primer incremento. Consiste en dibujar la serpiente de manera estática sin haber implementado ninguna funcionalidad

---
+++ Serpiente.cpp
@@ -0,0 +1,305 @@
+// Serpiente.cpp : Define el punto de entrada de la aplicación.
+//
+
+#include "framework.h"
+#include "Serpiente.h"
+
+#define MAX_LOADSTRING 100
+#define TAMSERP 20
+
+#define CUERPO  1
+#define CABEZA  2
+#define COLA    3
+
+#define IZQ 1
+#define DER 2
+#define ARRIBA  3
+#define ABAJO   4
+
+struct pos {
+    int x;
+    int y;
+};
+typedef struct pos POS;
+
+struct PedacitoS {
+    POS pos;
+    int tipo;
+    int dir;
+};
+typedef struct PedacitoS PEDACITOS;
+
+PEDACITOS* NuevaSerpiente(int);
+void DibujarSerpiente(HDC, const PEDACITOS *, RECT);
+
+// Variables globales:
+HINSTANCE hInst;                                // instancia actual
+WCHAR szTitle[MAX_LOADSTRING];                  // Texto de la barra de título
+WCHAR szWindowClass[MAX_LOADSTRING];            // nombre de clase de la ventana principal
+
+// Declaraciones de funciones adelantadas incluidas en este módulo de código:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Colocar código aquí.
+
+    // Inicializar cadenas globales
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_SERPIENTE, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Realizar la inicialización de la aplicación:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SERPIENTE));
+
+    MSG msg;
+
+    // Bucle principal de mensajes:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCIÓN: MyRegisterClass()
+//
+//  PROPÓSITO: Registra la clase de ventana.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SERPIENTE));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SERPIENTE);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCIÓN: InitInstance(HINSTANCE, int)
+//
+//   PROPÓSITO: Guarda el identificador de instancia y crea la ventana principal
+//
+//   COMENTARIOS:
+//
+//        En esta función, se guarda el identificador de instancia en una variable común y
+//        se crea y muestra la ventana principal del programa.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Almacenar identificador de instancia en una variable global
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCIÓN: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PROPÓSITO: Procesa mensajes de la ventana principal.
+//
+//  WM_COMMAND  - procesar el menú de aplicaciones
+//  WM_PAINT    - Pintar la ventana principal
+//  WM_DESTROY  - publicar un mensaje de salida y volver
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+    RECT rect = {NULL};
+    static PEDACITOS* serpiente = NULL;
+    static int tams = 5;
+
+    switch (message)
+    {
+    case WM_CREATE:
+        {
+            serpiente = NuevaSerpiente(tams);
+        }    
+        break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Analizar las selecciones de menú:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            hdc = BeginPaint(hWnd, &ps);
+
+            DibujarSerpiente(hdc, serpiente, rect);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Controlador de mensajes del cuadro Acerca de.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+PEDACITOS * NuevaSerpiente(int tams)
+{
+    PEDACITOS* serpiente = NULL;
+    int i;
+
+    if (tams < 2)
+        tams = 2;
+    serpiente = (PEDACITOS*)malloc(sizeof(PEDACITOS)*tams);
+    if (serpiente == NULL)
+    {
+        MessageBox(NULL, L"Sin memoria", L"Error", MB_OK | MB_ICONERROR);
+        exit(0);
+    }
+    serpiente[0].tipo = COLA;
+    serpiente[0].pos.x = 1;
+    serpiente[0].pos.y = 1;
+    serpiente[0].dir = DER;
+    for (i = 1; i < tams-1; i++)
+    {
+        serpiente[i].tipo = CUERPO;
+        serpiente[i].pos.x = i+1;
+        serpiente[i].pos.y = 1;
+        serpiente[i].dir = DER;
+    }
+    serpiente[i].tipo = CABEZA;
+    serpiente[i].pos.x = tams;
+    serpiente[i].pos.y = 1;
+    serpiente[i].dir = DER;
+
+    return serpiente;
+}
+void DibujarSerpiente(HDC hdc, const PEDACITOS *serpiente, RECT rect)
+{
+    int i = 1;
+    switch (serpiente[0].dir) {
+    case DER:
+        MoveToEx(hdc, serpiente[0].pos.x * TAMSERP + TAMSERP,
+            serpiente[0].pos.y * TAMSERP, NULL);
+        LineTo(hdc, serpiente[0].pos.x * TAMSERP,
+            serpiente[0].pos.y * TAMSERP + TAMSERP/2);
+        LineTo(hdc, serpiente[0].pos.x * TAMSERP + TAMSERP,
+            serpiente[0].pos.y * TAMSERP + TAMSERP);
+        LineTo(hdc, serpiente[0].pos.x * TAMSERP + TAMSERP,
+            serpiente[0].pos.y * TAMSERP);
+        break;
+    case IZQ:
+        break;
+    case ARRIBA:
+        break;
+    case ABAJO:
+        break;
+    }
+    while (serpiente[i].tipo != CABEZA) {
+        RoundRect(hdc, serpiente[i].pos.x * TAMSERP, 
+            serpiente[i].pos.y * TAMSERP,
+            serpiente[i].pos.x * TAMSERP + TAMSERP,
+            serpiente[i].pos.y * TAMSERP + TAMSERP,
+            5, 5);
+        i++;
+    }
+    RoundRect(hdc, serpiente[i].pos.x * TAMSERP,
+        serpiente[i].pos.y * TAMSERP,
+        serpiente[i].pos.x * TAMSERP + TAMSERP,
+        serpiente[i].pos.y * TAMSERP + TAMSERP,
+        5, 5);
+
+    switch (serpiente[i].dir)
+    {
+    case DER:
+        Ellipse(hdc, serpiente[i].pos.x * TAMSERP,
+            serpiente[i].pos.y * TAMSERP,
+            serpiente[i].pos.x * TAMSERP + TAMSERP/2,
+            serpiente[i].pos.y * TAMSERP + TAMSERP/2);
+        Ellipse(hdc, serpiente[i].pos.x * TAMSERP,
+            serpiente[i].pos.y * TAMSERP + TAMSERP/2,
+            serpiente[i].pos.x * TAMSERP + TAMSERP/2,
+            serpiente[i].pos.y * TAMSERP + TAMSERP);
+        break;
+    case IZQ:
+        break;
+    case ARRIBA:
+        break;
+    case ABAJO:
+        break;
+    }
+}
